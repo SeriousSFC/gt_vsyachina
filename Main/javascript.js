34c0d7b590981fdf9c0ee050a928f6e8ffc9b5ff
serioussfc <bloodx16.0@gmail.com>
2015-11-14 19:52:28 +0300

    Заполнены примерами все файлы + добавлен фон сайта

---
+++ Main/javascript.js
@@ -0,0 +1,41 @@
+var name,f,s,x;
+
+/*
+Скрипт расчета факториала-->
+var a,x,f,n;
+function fact (n)
+{
+	var p,i;
+	p=1;
+	for (i=1;i<=n;i++)
+	{
+		p=p*i;
+	}
+	return p;
+}
+x=prompt("Введите число для факториала"," ");
+a=fact(x);
+alert(a);
+<-- Конец скрипта факториала
+*/
+/* Тот же факториал через рекурсию
+x=prompt("Введите число для факториала"," ");
+function factrec(n)
+{
+	var p;
+	if (n==0)
+	{
+		p=1;
+	}
+	else
+	{
+		p=n*factrec(n-1);
+	}
+	return p;	
+}
+f=factrec(x);
+alert(f);
+*/
+name = prompt("Введите имя пользователя"," ");
+s = "Дарова, " + name;
+alert (s);
